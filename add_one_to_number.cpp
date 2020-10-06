8b4cc1d2c3226b0b2f843afd30e6c8ab2d28788e
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-10-06 15:54:14 +0530

    Create add_one_to_number.cpp

---
+++ add_one_to_number.cpp
@@ -0,0 +1,22 @@
+vector<int> Solution::plusOne(vector<int> &A) {
+    for(int i=A.size()-1;i>=0;i--){
+        int temp=A[i]+1;
+        if (temp>=10 && i>0){
+            A[i]=0;
+            continue;
+        }
+        else if(temp>=10 && i==0){
+            A[i]=0;
+            A.insert(A.begin(),1);
+            break;
+        }
+        else{
+            A[i]=temp;
+            break;
+        }
+    }
+    while(A[0]==0){
+        A.erase(A.begin());
+    }
+    return A;
+}
