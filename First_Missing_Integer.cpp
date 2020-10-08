f302d5638010a9940c40a760ed3c447ae9887181
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-10-08 21:57:32 +0530

    Create First_Missing_Integer.cpp

---
+++ First_Missing_Integer.cpp
@@ -0,0 +1,20 @@
+int Solution::firstMissingPositive(vector<int> &A) {
+    int n=A.size();
+    for(int i=0;i<A.size();i++){
+        if(A[i]>0 and A[i]<=n){
+            int pos=A[i]-1;
+            if(A[pos]!=A[i]){
+                int temp=A[pos];
+                A[pos]=A[i];
+                A[i]=A[pos];
+                i--;
+            }
+
+        }
+    }
+    for(int i=0;i<A.size();i++){
+        if(A[i]!=i+1)
+            return i+1;
+    }
+    return n+1;
+}
