c7026b2617235efc5544749edccc7f0e86a698ac
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-10-24 13:30:34 +0530

    Create Reverse_polish_notation.cpp

---
+++ Stacks&Queues/Reverse_polish_notation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& s) {
+        stack<int> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=="/" or s[i]=="+" or s[i]=="-" or s[i]=="*"){
+                int a=st.top();st.pop();
+                int b=st.top();st.pop();
+                    if(s[i] =="/")
+                        if(a==0)
+                            return 0;
+                        else
+                            st.push(b/a);
+                    else if(s[i]=="+")
+                        st.push(a+b);
+                    else if(s[i]=="*")
+                        st.push(a*b);
+                    else
+                        st.push(b-a);
+                
+            }
+            else{
+                st.push(stoi(s[i]));
+            }
+        }
+        return st.top();
+    }
+};
