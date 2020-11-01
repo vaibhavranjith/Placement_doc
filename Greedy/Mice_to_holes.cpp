a62d43865fc0c46b77b8735f1dbcd90249fa9c30
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-11-01 10:57:44 +0530

    Create Mice_to_holes.cpp

---
+++ Greedy/Mice_to_holes.cpp
@@ -0,0 +1,9 @@
+int Solution::mice(vector<int> &A, vector<int> &B) {
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    int mx=0;
+    for(int i=0;i<A.size();i++)
+        mx=max(abs(A[i]-B[i]),mx);
+    return mx;
+    
+}
