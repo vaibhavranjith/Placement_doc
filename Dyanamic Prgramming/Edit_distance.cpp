75e073b3d7edf2405fceb49c96cdf44fc867edbb
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-11-06 19:13:21 +0530

    Update Edit_distance.cpp

--- Dyanamic Prgramming/Edit_distance.cpp
+++ Dyanamic Prgramming/Edit_distance.cpp
@@ -1,15 +1,11 @@
-int dp[453][453];
-int tri_min(int a,int b,int c){
-    return min(min(a,b),c);
-}
-
 int Solution::minDistance(string A, string B) {
+    int dp[453][453];
     for(int i=1;i<=A.length();i++)  dp[0][i]=dp[0][i-1]+1;
     for(int i=1;i<=B.length();i++)  dp[i][0]=dp[i-1][0]+1;
     for(int i=1;i<=B.length();i++)
         for(int j=1;j<=A.length();j++)
             if(A[j-1]!=B[i-1])
-                dp[i][j]=tri_min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])+1;
+                dp[i][j]=min(min(dp[i][j-1],dp[i-1][j-1]),dp[i-1][j])+1;
             else
                 dp[i][j]=dp[i-1][j-1];
        
