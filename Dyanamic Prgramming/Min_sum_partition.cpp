f8302c7fbf9e96c52220d311876b0908b91fe9b8
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-11-07 14:12:05 +0530

    Update Min_sum_partition.cpp

--- Dyanamic Prgramming/Min_sum_partition.cpp
+++ Dyanamic Prgramming/Min_sum_partition.cpp
@@ -1,14 +1,10 @@
-	int minDiffernce(int arr[], int n) 
-	{ 
-	    int sum=0;
-	    for(int i=0;i<n;i++) 
-	        sum+=arr[i];
-	        
-	    bool dp[n+1][sum+1];
-	    
-	    for(int i=0;i<=n;i++){
-	        
-	        for(int j=0;j<=sum;j++){
+int minDiffernce(int arr[], int n) { 
+	int sum=0;
+	for(int i=0;i<n;i++) 
+		sum+=arr[i];      
+	bool dp[n+1][sum+1];
+	for(int i=0;i<=n;i++)
+		for(int j=0;j<=sum;j++){
 	            if(j==0)
 	               dp[i][j]=true;
 	            else if(i==0)
@@ -18,13 +14,9 @@
 	            else 
 	                dp[i][j]=dp[i-1][j] || dp[i-1][j-arr[i-1]];
 	        }
-	    }
-	    
-	    int diff=INT_MAX;
-	    
-	    for(int i=0;i<=sum/2;i++)
-	        if(dp[n][i] and diff>abs(sum-2*i))
-	            diff=abs(sum-2*i);
-	            
-	   return diff;
-	} 
+	int diff=INT_MAX;
+	for(int i=0;i<=sum/2;i++)
+		if(dp[n][i] and diff>abs(sum-2*i))
+	        	diff=abs(sum-2*i);
+	return diff;
+} 
