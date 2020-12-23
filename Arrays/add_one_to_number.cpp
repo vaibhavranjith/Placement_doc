6ed192eb73dfcdd03287ef2c2cc4a54280f30ec9
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-23 10:03:14 +0530

    Update add_one_to_number.cpp

--- Arrays/add_one_to_number.cpp
+++ Arrays/add_one_to_number.cpp
@@ -1,22 +1,17 @@
-vector<int> Solution::plusOne(vector<int> &A) {
-     for(int i=A.size()-1;i>=0;i--){
-        int temp=A[i]+1;
-        if (temp>=10 && i>0){
-            A[i]=0;
-            continue;
+vector<int> Solution::plusOne(vector<int> &arr) {
+    int c=0;
+    int n=arr.size();
+    arr[n-1]=arr[n-1]+1;
+    c=arr[n-1]/10;
+    arr[n-1]=arr[n-1]%10;
+    for(int i=n-2;i>=0;i--)
+        if(c==1){
+            arr[i]=arr[i]+1;
+            c=arr[i]/10;
+            arr[i]=arr[i]%10;
         }
-        else if(temp>=10 && i==0){
-            A[i]=0;
-            A.insert(A.begin(),1);
-            break;
-        }
-        else{
-            A[i]=temp;
-            break;
-        }
-    }
-    while(A[0]==0){
-        A.erase(A.begin());
-    }
-    return A;
+    if(c==1)
+        arr.insert(arr.begin(),1);
+    while(arr[0]==0)arr.erase(arr.begin());
+    return arr;
 }
