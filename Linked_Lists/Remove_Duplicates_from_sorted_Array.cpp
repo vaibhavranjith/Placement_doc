c05b0408b4e8c27ded7c2edb191fe1eb5e3ef828
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-24 20:43:49 +0530

    Update Remove_Duplicates_from_sorted_Array.cpp

--- Linked_Lists/Remove_Duplicates_from_sorted_Array.cpp
+++ Linked_Lists/Remove_Duplicates_from_sorted_Array.cpp
@@ -1,11 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 ListNode* Solution::deleteDuplicates(ListNode* head) {
     if(head==NULL || head->next==NULL)
         return head;
