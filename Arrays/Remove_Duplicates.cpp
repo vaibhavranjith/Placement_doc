6e0f501aa138d722fc4a27f8906452cd4e5ae96b
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-24 12:06:23 +0530

    Update Remove_Duplicates.cpp

--- Arrays/Remove_Duplicates.cpp
+++ Arrays/Remove_Duplicates.cpp
@@ -1,9 +1,8 @@
 int Solution::removeDuplicates(vector<int> &A) {
     int index=1;
-    for(int i=0;i<A.size()-1;i++){
+    for(int i=0;i<A.size()-1;i++)
         if(A[i]!=A[i+1]){
             A[index++]=A[i+1];
         }
-    }
    return index;
 }
