4d61306e4be4977e00423cd8319dbe2535f84a56
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-24 16:36:39 +0530

    Update Container_with_most_water.cpp

--- Arrays/Container_with_most_water.cpp
+++ Arrays/Container_with_most_water.cpp
@@ -1,16 +1,13 @@
 int Solution::maxArea(vector<int> &A) {
-    int max_area=0;
-    int pt1=0;
-    int pt2=A.size()-1;
-    while(pt1<pt2){
-        if(A[pt1]<A[pt2]){
-            max_area=max(max_area,A[pt1]*(pt2-pt1));
-            pt1++;
+    int max_area=0,i=0,j=A.size()-1;
+    while(i<j)
+        if(A[i]<A[j]){
+            max_area=max(max_area,A[i]*(j-i));
+            i++;
         }
         else{
-            max_area=max(max_area,A[pt2]*(pt2-pt1));
-            pt2--;
+            max_area=max(max_area,A[j]*(j-i));
+            j--;
         }
-    }
     return max_area;
 }
