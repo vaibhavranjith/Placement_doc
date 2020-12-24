46702e195c66d9691580dbadab00ec96f3cfa6d2
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-24 12:41:27 +0530

    Update Grid_Unique_Paths.cpp

--- Arrays/Grid_Unique_Paths.cpp
+++ Arrays/Grid_Unique_Paths.cpp
@@ -1,13 +1,11 @@
 
 int Solution::uniquePaths(int A, int B) {
     vector<vector<int>> dp(A,vector<int>(B,0));
-    for(int i=0;i<A;i++){
-        for(int j=0;j<B;j++){
+    for(int i=0;i<A;i++)
+        for(int j=0;j<B;j++)
             if(i==0 || j==0)
                 dp[i][j]=1;
-            else
-                dp[i][j]=dp[i-1][j] +dp[i][j-1];
-        }
-    }
+            else 
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
     return dp[A-1][B-1];
 }
