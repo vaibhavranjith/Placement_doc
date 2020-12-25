d96ff9e8208acc4fa64ca94ef7891c17058421bc
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-25 10:28:27 +0530

    Update Rotate_List.cpp

--- Linked_Lists/Rotate_List.cpp
+++ Linked_Lists/Rotate_List.cpp
@@ -1,13 +1,12 @@
 
 ListNode* Solution::rotateRight(ListNode* A, int B) {
     if(A==NULL || A->next==NULL)
-        return  A;
+        return A;
     int n=0;
-    ListNode *tr=A;
-    ListNode *dr=NULL;
-    while(tr!=NULL){ n++; tr=tr->next;}
-    int l= (n-B)>0 ? n-B : n-(B%n);
-    if(l==n) 
+    ListNode* tr=A;
+    while(tr!=NULL){n++;tr=tr->next;}
+    int l=n-(B%n);
+    if(l==n)
         return A;
     n=0;
     tr=A;
@@ -15,11 +14,11 @@ ListNode* Solution::rotateRight(ListNode* A, int B) {
         n++;
         tr=tr->next;
     }
-    dr=tr->next;
-    ListNode *tr2=dr;
+    ListNode* rt=tr->next;
+    ListNode* temp=rt;
     tr->next=NULL;
-    while(tr2->next!=NULL)
-        tr2=tr2->next;
-    tr2->next=A;
-    return dr;
+    while(temp->next!=NULL)
+        temp=temp->next;
+    temp->next=A;
+    return rt;
 }
