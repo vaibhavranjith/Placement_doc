51f093fdbc94f580298754bab4111b0d18228022
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-25 18:54:10 +0530

    Update Balanced_brackets.cpp

--- Stacks&Queues/Balanced_brackets.cpp
+++ Stacks&Queues/Balanced_brackets.cpp
@@ -1,54 +1,28 @@
-#include <iostream>
-#include <bits/stdc++.h> 
-using namespace std;
-
-
-bool balanced(string brack){
+bool ispar(string x)
+{
     unordered_map<char,bool> mp={
-    {'{',true},
-    {'[',true},
-    {'(',true},
-    {'}',false},
-    {']',false},
-    {')',false}
+        {'(',true},
+        {'{',true},
+        {'[',true},
+        {')',false},
+        {']',false},
+        {'}',false}
     };
-    unordered_map<char,char> mp2={
-    {'}','{'},
-    {')','('},
-    {']','['}
+    unordered_map<char,char> mp1={
+        {'}','{'},
+        {')','('},
+        {']','['}
     };
     stack<char> pen;
-    for(int i=0;i<brack.size();i++){
-        if(mp[brack[i]])
-            pen.push(brack[i]);
+    for(int i=0;i<x.size();i++)
+        if(mp[x[i]])
+            pen.push(x[i]);
         else{
-            if(pen.empty())
-                return false;
-            else{
-                if(pen.top()==mp2[brack[i]]){
-                    pen.pop();
-                    continue;
-                }
-                return false;
+            if(!pen.empty() and pen.top()==mp1[x[i]]){
+                pen.pop();
+                continue;
             }
+            return false;
         }
-    }
-    if(!pen.empty())
-        return false;
-    return true;
- 
-}
-int main() {
-	//code
-	string in;
-    int t=0;
-    cin>>t;
-    while(t-->0){
-        cin>>in;
-        if(balanced(in))
-            cout<<"balanced\n";
-        else
-            cout<<"not balanced\n";
-    }
-	return 0;
+    return pen.empty() ? true:false;
 }
