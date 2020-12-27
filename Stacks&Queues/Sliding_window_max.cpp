b7ab8c69582a86b5e193a55fc67c34ed84622ec4
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-27 17:19:58 +0530

    Update Sliding_window_max.cpp

--- Stacks&Queues/Sliding_window_max.cpp
+++ Stacks&Queues/Sliding_window_max.cpp
@@ -1,22 +1,22 @@
 vector<int> Solution::slidingMaximum(const vector<int> &arr, int k) {
     int n=arr.size();
-    std::deque<int> Qi(k); 
-    int i; 
-    for (i = 0; i < k; ++i) { 
-        while ((!Qi.empty()) && arr[i] >= arr[Qi.back()]) 
-            Qi.pop_back(); 
-        Qi.push_back(i); 
-    } 
+    deque<int> q(k);
+    int i=0;
+    for(i=0;i<k;i++){
+        while(!q.empty() and arr[i]>=arr[q.back()])
+            q.pop_back();
+        q.push_back(i);
+    }
     vector<int> ret;
-    for (; i < n; ++i) { 
-        ret.push_back(arr[Qi.front()]);
-        while ((!Qi.empty()) && Qi.front() <= i - k) 
-            Qi.pop_front();
-        while ((!Qi.empty()) && arr[i] >= arr[Qi.back()]) 
-            Qi.pop_back(); 
-        Qi.push_back(i); 
-    } 
-    ret.push_back(arr[Qi.front()]);
+    for(;i<n;i++){
+        ret.push_back(arr[q.front()]);
+        while(!q.empty() and q.front()<=i-k)
+            q.pop_front();
+        while(!q.empty() and arr[i]>=arr[q.back()])
+            q.pop_back();
+        q.push_back(i);
+    }
+    ret.push_back(arr[q.front()]);
     return ret;
 }
 
