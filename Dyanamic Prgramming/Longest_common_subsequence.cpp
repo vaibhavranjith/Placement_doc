084bc342f0733df7898a110954ee10863328ec44
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-27 21:37:14 +0530

    Update Longest_common_subsequence.cpp

--- Dyanamic Prgramming/Longest_common_subsequence.cpp
+++ Dyanamic Prgramming/Longest_common_subsequence.cpp
@@ -1,11 +1,10 @@
 int dp[1007][1007];
 int Solution::solve(string A, string B) {
-    for(int i=1;i<=A.length();i++){
+    for(int i=1;i<=A.length();i++)
         for(int j=1;j<=B.length();j++)
             if(A[i-1]==B[j-1])
                 dp[i][j]=dp[i-1][j-1]+1;
             else
                 dp[i][j]= dp[i-1][j]>dp[i][j-1]?dp[i-1][j]:dp[i][j-1];
-    }
     return dp[A.length()][B.length()];
 }
