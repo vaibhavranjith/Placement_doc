f286e371fd334c2ceb010a502954e9ed427867ff
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-30 10:22:01 +0530

    Update Longest_increasing_subsequence.cpp

--- Dyanamic Prgramming/Longest_increasing_subsequence.cpp
+++ Dyanamic Prgramming/Longest_increasing_subsequence.cpp
@@ -1,22 +1,9 @@
 //deep dive in b2b swe https://www.youtube.com/watch?v=fV-TF4OvZpk
-int maximum_ele(vector<int> dp,int n){
-    int ret=0;
-    for(int i=0;i<n;i++)
-        ret=dp[i]>ret? dp[i]:ret;
-    return ret;
-}
 int Solution::lis(const vector<int> &A) {
     vector<int> dp(A.size(),1);
-    int i=0;
-    int j=1;
-    while(j<A.size()){
-        for(int i=0;i<j;i++){
-            if(A[j]>A[i])
-                dp[j]=(dp[i]+1)>dp[j]?dp[i]+1:dp[j];
-            else
-                continue;
-        }
-        j++;
-    }
-    return maximum_ele(dp,A.size());
+    for(int i=1;i<A.size();i++)
+        for(int j=0;j<i;j++)
+            if(A[i]>A[j])
+                dp[i]=dp[j]+1>dp[i]? dp[j]+1:dp[i];
+    return *max_element(dp.begin(),dp.end());
 }
