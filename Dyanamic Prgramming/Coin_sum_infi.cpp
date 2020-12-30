6311da5b16d583a00265f0dddd2cea68f9a3125a
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2020-12-30 20:54:28 +0530

    Update Coin_sum_infi.cpp

--- Dyanamic Prgramming/Coin_sum_infi.cpp
+++ Dyanamic Prgramming/Coin_sum_infi.cpp
@@ -1,10 +1,8 @@
-
 int Solution::coinchange2(vector<int> &A, int B) {
-    long table[B+1]; 
-    memset(table, 0, sizeof(table)); 
-    table[0] = 1; 
-    for(int i=0; i<A.size(); i++) 
-        for(int j=A[i]; j<=B; j++) 
-            table[j] += table[j-A[i]]%1000007; 
-    return table[B]% 1000007; 
+    vector<int> table(B+1,0);
+    table[0]=1;
+    for(int i=0;i<A.size();i++)
+        for(int j=A[i];j<=B;j++)
+            table[j]+=table[j-A[i]]%1000007;
+    return table[B]%1000007;
 }
