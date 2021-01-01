f5d4b2c1aeeb4a0e0060e21ca608b3395c5ee0c6
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-01 20:58:15 +0530

    Update Coin_sum_infi.cpp

--- Dyanamic Prgramming/Coin_sum_infi.cpp
+++ Dyanamic Prgramming/Coin_sum_infi.cpp
@@ -1,3 +1,5 @@
+// explanation on this chat :   https://www.hackerrank.com/challenges/ctci-coin-change/forum
+// permalink to the comment:    https://www.hackerrank.com/challenges/ctci-coin-change/forum/comments/211697
 int Solution::coinchange2(vector<int> &A, int B) {
     vector<int> table(B+1,0);
     table[0]=1;
