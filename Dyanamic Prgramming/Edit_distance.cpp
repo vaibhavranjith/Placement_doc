1e7a03eb16b4e02c06794b0d93ec2307c746e516
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-02 10:57:36 +0530

    Update Edit_distance.cpp

--- Dyanamic Prgramming/Edit_distance.cpp
+++ Dyanamic Prgramming/Edit_distance.cpp
@@ -2,8 +2,8 @@
 // | Delete    X    |
 int Solution::minDistance(string A, string B) {
     int dp[453][453];
-    for(int i=0;i<A.length();i++) dp[i][0]=i;
-    for(int j=0;j<B.length();j++) dp[0][j]=j;
+    for(int i=0;i<=A.length();i++) dp[i][0]=i;
+    for(int j=0;j<=B.length();j++) dp[0][j]=j;
     for(int i=1;i<=B.length();i++)
         for(int j=1;j<=A.length();j++)
             if(A[j-1]!=B[i-1])
