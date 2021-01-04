bd1e92894f2d181f9b33d8757bf13eb73c4244db
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-04 18:22:57 +0530

    Update sum_of_factors.cpp

--- practice_test_II/sum_of_factors.cpp
+++ practice_test_II/sum_of_factors.cpp
@@ -17,3 +17,26 @@ int sumofFactors(int n) {
         res *= (1 + n);       // if the number is a prime.
     return res; 
 } 
+//method 2: This is the answer!
+
+int main(){
+    int q;
+    cin>>q;
+    vector<long long int> sieve(1000001);
+    sieve[1]=1;
+    for(int i=2;i<seive.size();i++)
+        sieve[i]=1+i;
+    for(long long int i=2;i*i<=1000000;i++){
+        seive[i*i]+=i;
+        for(long long j=i*i+i;j<1000000;j+=i){
+            sieve[i]+=i;
+            sieve[i]+=j/i;
+        }
+    }
+    while(q--){
+        int x;
+        cin>>x;
+        cout<<sieve[x]<<endl;
+    }
+    return 0;
+}
