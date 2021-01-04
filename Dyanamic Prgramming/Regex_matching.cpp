a2e34e7090238bd833c3f9c567203a0d19fc3ef7
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-04 14:51:37 +0530

    Update Regex_matching.cpp

--- Dyanamic Prgramming/Regex_matching.cpp
+++ Dyanamic Prgramming/Regex_matching.cpp
@@ -1,15 +1,16 @@
-bool isMatch(string s, string p) {
-        vector<vector<bool>> memo(32,vector<bool>(32,false));
-        memo[s.length()][p.length()]=true;
-        
-        for(int i=s.length();i>=0;i--){
-            for(int j=p.length()-1;j>=0;j--){
-                bool fm=(i < s.length() and (p[j]==s[i] or p[j]=='.'));
-                if(j+1<p.length() and p[j+1]=='*')
-                    memo[i][j] = (memo[i][j+2] or (fm and memo[i+1][j]));
-                else 
-                    memo[i][j]= (fm and memo[i+1][j+1]);
-            }
-        }
-        return memo[0][0];
+int Solution::isMatch(const string s, const string p) {
+    vector<vector<bool>> dp(s.length()+1,vector<bool>(p.length()+1,false));
+    dp[0][0]=true;
+    for(int i=1;i<=p.length();i++)
+        if(p[i-1]=='*')
+            dp[0][i]=dp[0][i-1];
+    for(int i=1;i<=s.length();i++)
+        for(int j=1;j<=p.length();j++)
+            if(p[j-1]=='*')
+                dp[i][j]=dp[i][j-1] || dp[i-1][j];
+            else if(p[j-1]=='?' || p[j-1]==s[i-1])
+                dp[i][j]=dp[i-1][j-1];
+            else
+                dp[i][j]=false;
+    return dp[s.length()][p.length()] ? 1:0;
 }
