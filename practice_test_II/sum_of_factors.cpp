6537d57e1604a84a9b3cebf4653112310f2808c8
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-04 11:48:46 +0530

    Update sum_of_factors.cpp

--- practice_test_II/sum_of_factors.cpp
+++ practice_test_II/sum_of_factors.cpp
@@ -1,1 +1,17 @@
-find the sum of factors for each element including 1 and itself.
+// find the sum of factors for each element including 1 and itself.
+int sumofFactors(int n) { 
+    int res = 1; 
+    for (int i = 2; i <= sqrt(n); i++) {
+        int curr_sum = 1; 
+        int curr_term = 1; 
+        while (n % i == 0) { 
+            n = n / i; //we are reducing the limit here 
+            curr_term *= i; 
+            curr_sum += curr_term; 
+        } 
+        res *= curr_sum; 
+    } 
+    if (n >= 2) 
+        res *= (1 + n);       // if the number is a prime.
+    return res; 
+} 
