0e623a29d54ece62d3528a0c687fb9aa13ff419d
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-06 17:25:14 +0530

    Update substr_of_vowels.cpp

--- meesho/substr_of_vowels.cpp
+++ meesho/substr_of_vowels.cpp
@@ -1,1 +1,25 @@
-https://www.geeksforgeeks.org/find-substrings-contain-vowels/
+//https://www.geeksforgeeks.org/find-substrings-contain-vowels/
+bool isVowel(char x) 
+{ 
+    return (x == 'a' || x == 'e' || x == 'i' || 
+                        x == 'o' || x == 'u'); 
+} 
+void FindSubstring(string str) 
+{ 
+    set<char> hash;  // To store vowels 
+    int start = 0; 
+    for (int i=0; i<str.length(); i++) 
+        if (isVowel(str[i]) == true) 
+        { 
+            hash.insert(str[i]); 
+            if (hash.size()==5) 
+                cout << str.substr(start, i-start+1) 
+                     << " "; 
+        } 
+        else
+        { 
+            start = i+1; 
+            hash.clear(); 
+        } 
+} 
+  
