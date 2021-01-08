2609a34783f372e6a658ae7c3797b7fbb0fde287
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-08 19:46:36 +0530

    Update grey_codes.cpp

--- backtracking/grey_codes.cpp
+++ backtracking/grey_codes.cpp
@@ -1,3 +1,8 @@
+// While this is a good observation. If the question was asking for the bit vector instead of the number itself the observation to make is
+// that, if :
+//      1. L1 represents all the bit patterns, to generate the n vector reverse L1 and let it be L2  
+//      2. Append 0 to all patterns in L1 and append 1 to all patterns in L2 now concatenate these vectors.
+
 vector<int> grayCode(int n) {
         if(n==0) return {};
         vector<int> ans={0,1};
