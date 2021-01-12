081ef1407f7a0adc8772e6d5eabfd8902cd4ea42
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-12 15:22:01 +0530

    Update combination_iterator.cpp

--- backtracking/combination_iterator.cpp
+++ backtracking/combination_iterator.cpp
@@ -1,10 +1,15 @@
-CombinationIterator(string characters, int combinationLength) {
+class CombinationIterator {
+    string s,prev;
+    int n;
+public:
+    CombinationIterator(string characters, int combinationLength) {
         s=characters;
         n=combinationLength;
         prev="";
         hasNext();
-}
-string next() {
+    }
+    
+    string next() {
         if(prev == ""){
             prev = s.substr(0, n);
             return prev;
@@ -13,12 +18,21 @@ string next() {
         while(prev[i--] == s[j--]) ++count;
         prev = prev.substr(0, n - count) + s.substr(s.find(prev[n - count]) + 1, count);
         return prev;
-}  
-bool hasNext() {
+    }
+    
+    bool hasNext() {
        int count = 0, i = prev.size() - 1, j = s.size() - 1;
        cout<<i<<" "<<j;
        while(prev[i--] == s[j--]) 
            ++count;
        if(count == n) return false;
       return true;
-}
+    }
+};
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator* obj = new CombinationIterator(characters, combinationLength);
+ * string param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
