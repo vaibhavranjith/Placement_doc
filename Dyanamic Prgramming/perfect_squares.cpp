f8a83082fdb457bb816456cf953782b3135c297c
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-14 13:55:52 +0530

    Update perfect_squares.cpp

--- Dyanamic Prgramming/perfect_squares.cpp
+++ Dyanamic Prgramming/perfect_squares.cpp
@@ -12,3 +12,28 @@ int numSquares(int n) {
                 }         
         return dp[n];    
 }
+
+
+
+// Legendre theorem based very fast and uses way less memory: brilliant !!!
+
+int numSquares(int n) {
+        //we will use the  legendre's theorem. The answer will be betwen 1-4. 4 when the number is of the form of : 4a(8b+7).
+        //check if n is a perfect  square
+        int a =sqrt(n);
+        if(a*a==n)
+            return 1;
+        //check for condition to have 4 numbers
+        while(n%4==0)
+            n/=4; // This also decreases the search space for the loop below.
+        if(n%8==7)
+            return 4;
+        //check for condition for 2 factors. we can work with this reduced value of n.
+        for(int i=1;i*i<=n;i++)
+        {
+            int y=sqrt(n-(i*i));
+            if(y*y==(n-i*i))
+                return 2;
+        }
+        return 3;
+    }
