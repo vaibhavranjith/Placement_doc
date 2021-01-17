6060251781bcc30d27de63921b6804a37b164876
Vaibhav Ranjith <30415345+vaibhavranjith@users.noreply.github.com>
2021-01-17 16:40:52 +0530

    Update Longest_increasing_subsequence.cpp

--- Dyanamic Prgramming/Longest_increasing_subsequence.cpp
+++ Dyanamic Prgramming/Longest_increasing_subsequence.cpp
@@ -6,3 +6,17 @@ int Solution::lis(const vector<int> &A) {
                 dp[i]=dp[j]+1>dp[i]? dp[j]+1:dp[i];
     return *max_element(dp.begin(),dp.end());
 }
+
+// Just realised this is a very slow dynamic programming algorithm though intutive... This algorith decreased time from 492ms to 12ms: credits to a leetcoder
+
+int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp;
+        for(int i = 0; i < nums.size(); i++){
+            auto it = lower_bound(dp.begin(),dp.end(),nums[i]);
+            if(it == dp.end()) 
+                dp.push_back(nums[i]);
+            else 
+                dp[it-dp.begin()] = nums[i];
+        }
+        return dp.size();
+}
